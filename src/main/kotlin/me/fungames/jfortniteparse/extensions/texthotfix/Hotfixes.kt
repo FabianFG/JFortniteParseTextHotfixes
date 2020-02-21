5ee025b648341a3b7c8790333883082ef8538d8c
FunGamesLeaks <mcdtloeser@gmail.com>
2020-02-21 19:54:10 +0100

    using fortnite api now

--- src/main/kotlin/me/fungames/jfortniteparse/extensions/texthotfix/Hotfixes.kt
+++ src/main/kotlin/me/fungames/jfortniteparse/extensions/texthotfix/Hotfixes.kt
@@ -1,16 +1,12 @@
 package me.fungames.jfortniteparse.extensions.texthotfix
 
+import me.fungames.fortnite.api.FortniteApi
+import me.fungames.fortnite.api.model.CloudStorageResponse
 import me.fungames.jfortniteparse.ue4.locres.FnLanguage
 import me.fungames.jfortniteparse.ue4.locres.Locres
-import okhttp3.ResponseBody
 import org.ini4j.Ini
-import retrofit2.Call
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.*
-import java.util.concurrent.TimeUnit
-import kotlin.properties.Delegates
 
+@Suppress("EXPERIMENTAL_API_USAGE")
 fun Locres.applyHotfixes(hotfixes: HotfixData) {
     hotfixes.data.forEach { (namespace, data) -> data.forEach { (key, values) ->
         val text = values[language]
@@ -24,18 +20,9 @@ fun Locres.applyHotfixes(hotfixes: HotfixData) {
 fun Locres.applyHotfixes() = this.applyHotfixes(Hotfixes.hotfixes)
 
 object Hotfixes {
-    private val retrofit = Retrofit.Builder()
-        .addConverterFactory(
-            GsonConverterFactory.create())
-        .baseUrl("https://account-public-service-prod.ol.epicgames.com")
-        .build()
-    private val accountPublicService = retrofit.create(AccountPublicService::class.java)!!
-    private val fortnitePublicService = retrofit.newBuilder().baseUrl("https://fortnite-public-service-prod11.ol.epicgames.com").build().create(FortnitePublicService::class.java)!!
+    private val api = FortniteApi.Builder().loginAsUser(false).buildAndLogin()
 
-    private lateinit var token : String
-    private var tokenExpire by Delegates.notNull<Long>()
-
-    private var expire = 0L
+    var expire = 0L
     private lateinit var hotfixData : HotfixData
     val hotfixes: HotfixData
         get() {
@@ -49,46 +36,17 @@ object Hotfixes {
         expire = System.currentTimeMillis() + 1800000
     }
 
-    private fun login(): Boolean {
-        if (::token.isInitialized)
-            return refreshToken()
-        val resp = accountPublicService.grantToken("basic MzQ0NmNkNzI2OTRjNGE0NDg1ZDgxYjc3YWRiYjIxNDE6OTIwOWQ0YTVlMjVhNDU3ZmI5YjA3NDg5ZDMxM2I0MWE", "client_credentials", emptyMap(), null).execute()
-        if (!resp.isSuccessful)
-            return false
-        this.token = resp.body()?.access_token ?: return false
-        this.tokenExpire = System.currentTimeMillis() + (resp.body()?.expires_in ?: return false)
-        return true
-    }
-
-    private fun refreshToken() : Boolean {
-        val resp = accountPublicService.grantToken("basic MzQ0NmNkNzI2OTRjNGE0NDg1ZDgxYjc3YWRiYjIxNDE6OTIwOWQ0YTVlMjVhNDU3ZmI5YjA3NDg5ZDMxM2I0MWE", "client_credentials", emptyMap(), null).execute()
-        if (!resp.isSuccessful)
-            return false
-        this.token = resp.body()?.access_token ?: return false
-        this.tokenExpire = System.currentTimeMillis() + (resp.body()?.expires_in ?: return false)
-        return true
-    }
-
-    private fun verifyToken() : Boolean {
-        if (!::token.isInitialized)
-            return login()
-        else if (System.currentTimeMillis() > TimeUnit.MILLISECONDS.convert(tokenExpire, TimeUnit.SECONDS))
-            return refreshToken()
-        return true
-    }
-
     private fun getGameIni() : Ini {
         val file = getFileList().first { it.filename == "DefaultGame.ini" }
-        val data = fortnitePublicService.downloadCloudstorageFile("bearer $token", file.uniqueFilename).execute().body()?.bytes() ?: throw IllegalStateException("Couldn't get defaultgame.ini")
+        val data = api.fortnitePublicService.downloadCloudstorageFile(file.uniqueFilename).execute().body()?.bytes() ?: throw IllegalStateException("Couldn't get defaultgame.ini")
         val ini = Ini()
         ini.config.isMultiSection = true
         ini.load(data.inputStream())
         return ini
     }
 
-    private fun getFileList() : List<Model.CloudStorageResponse> {
-        check(verifyToken()) { "Could not login into fortnite api" }
-        val resp = fortnitePublicService.getCloudstorageList("bearer $token").execute()
+    private fun getFileList() : List<CloudStorageResponse> {
+        val resp = api.fortnitePublicService.getCloudstorageList().execute()
         return resp.body() ?: throw IllegalStateException("Couldn't get cloudstorage list")
     }
 
@@ -122,58 +80,6 @@ object Hotfixes {
 }
 
 class HotfixData internal constructor(val data : Map<String, Map<String, Map<FnLanguage, String>>>)
-
-private interface AccountPublicService {
-    /**
-     * grant_type: authorization_code; fields: code
-     * grant_type: client_credentials -- used to retrieve client ID without sign in
-     * grant_type: device_auth; fields: account_id, device_id, secret
-     * grant_type: exchange_code; fields: exchange_code
-     * grant_type: external_auth; fields: external_auth_type, external_auth_token
-     * grant_type: otp; fields: otp, challenge
-     * grant_type: password; fields: username, password
-     * grant_type: refresh_token; fields: refresh_token
-     */
-    @FormUrlEncoded
-    @POST("/account/api/oauth/token")
-    fun grantToken(
-        @Header("Authorization") auth: String, @Field("grant_type") grantType: String, @FieldMap fields: Map<String, String>, @Field(
-            "includePerms"
-        ) includePerms: Boolean?
-    ): Call<Model.LoginResponse>
-}
-
-private interface FortnitePublicService {
-    @GET("/fortnite/api/cloudstorage/system")
-    fun getCloudstorageList(@Header("Authorization") auth: String) : Call<List<Model.CloudStorageResponse>>
-    @GET("/fortnite/api/cloudstorage/system/{uniqueFilename}")
-    fun downloadCloudstorageFile(@Header("Authorization") auth: String, @Path("uniqueFilename") uniqueFilename: String) : Call<ResponseBody>
-}
-
-private object Model {
-    data class LoginResponse (
-
-        val access_token : String,
-        val expires_in : Int,
-        val expires_at : String,
-        val token_type : String,
-        val client_id : String,
-        val internal_client : Boolean,
-        val client_service : String
-    )
-
-    data class CloudStorageResponse (
-        val uniqueFilename : String,
-        val filename : String,
-        val hash : String,
-        val hash256 : String,
-        val length : Int,
-        val contentType : String,
-        val uploaded : String,
-        val storageType : String,
-        val doNotCache : Boolean
-    )
-}
 
 object LocresTextReplacementParser {
 
